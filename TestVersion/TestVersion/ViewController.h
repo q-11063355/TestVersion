43063270f2c2cb61ca82367de583feef2830b268
航哥 <11063355@qq.com>
2018-05-23 13:18:10 +0800

    no message

---
+++ TestVersion/TestVersion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestVersion
+//
+//  Created by 航哥 on 2018/5/23.
+//  Copyright © 2018年 com.JuFan.Merchant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestVersion/TestVersion/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestVersion
+//
+//  Created by 航哥 on 2018/5/23.
+//  Copyright © 2018年 com.JuFan.Merchant. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
